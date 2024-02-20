c5425cd8f32acaef43bb4e26ccbc80bf2166872d
zoff99 <zoff99@users.noreply.github.com>
2024-02-20 20:39:36 +0100

    WIP: rust jni notification lib for linux

---
+++ .localrun/docker_it_rsub20.sh
@@ -0,0 +1,96 @@
+#! /bin/bash
+
+_HOME2_=$(dirname $0)
+export _HOME2_
+_HOME_=$(cd $_HOME2_;pwd)
+export _HOME_
+
+echo $_HOME_
+cd $_HOME_
+
+if [ "$1""x" == "buildx" ]; then
+    docker build -f Dockerfile_rsub20 -t jni_notifications_ready_rsub20_001 .
+    exit 0
+fi
+
+build_for='
+rust:20.04
+'
+
+for system_to_build_for in $build_for ; do
+
+    system_to_build_for_orig="$system_to_build_for"
+    system_to_build_for=$(echo "$system_to_build_for_orig" 2>/dev/null|tr ':' '_' 2>/dev/null)
+
+    cd $_HOME_/
+    mkdir -p $_HOME_/"$system_to_build_for"/
+
+    mkdir -p $_HOME_/"$system_to_build_for"/artefacts
+    mkdir -p $_HOME_/"$system_to_build_for"/script
+    mkdir -p $_HOME_/"$system_to_build_for"/workspace/build/
+
+    ls -al $_HOME_/"$system_to_build_for"/
+
+    rsync -av ../jni_notifications.rs ../Cargo.toml ../src --exclude=.localrun $_HOME_/"$system_to_build_for"/workspace/build/
+    chmod a+rwx -R $_HOME_/"$system_to_build_for"/workspace/build >/dev/null 2>/dev/null
+
+    echo '#! /bin/bash
+
+cd /workspace/build/
+
+_HOME_="$(pwd)"
+export _HOME_
+
+cd "$_HOME_"
+
+export _SRC_=$_HOME_/src/
+export _INST_=$_HOME_/inst/
+
+mkdir -p $_SRC_
+mkdir -p $_INST_
+
+echo "*** compile ***"
+
+cd /workspace/build/
+gcc --version
+
+ls -al
+
+source "$HOME/.cargo/env"
+CARGO_TARGET_DIR=./outlib cargo build --release
+
+find . -name "*so"
+
+ldd ./outlib/release/libjni_notifications.so
+ls -hal ./outlib/release/libjni_notifications.so
+
+cp -av ./outlib/release/libjni_notifications.so libjni_notifications.so
+
+ls -al libjni_notifications.so || exit 1
+pwd
+file libjni_notifications.so
+cp -v libjni_notifications.so /artefacts/ || exit 1
+
+chmod a+rw /artefacts/*
+
+' > $_HOME_/"$system_to_build_for"/script/run.sh
+
+    mkdir -p $_HOME_/"$system_to_build_for"/workspace/build/c-toxcore/
+
+    docker run -ti --rm \
+      -v $_HOME_/"$system_to_build_for"/artefacts:/artefacts \
+      -v $_HOME_/"$system_to_build_for"/script:/script \
+      -v $_HOME_/"$system_to_build_for"/workspace:/workspace \
+      --net=host \
+     "jni_notifications_ready_rsub20_001" \
+     /bin/sh -c "apk add bash >/dev/null 2>/dev/null; /bin/bash /script/run.sh"
+     if [ $? -ne 0 ]; then
+        echo "** ERROR **:$system_to_build_for_orig"
+        exit 1
+     else
+        echo "--SUCCESS--:$system_to_build_for_orig"
+     fi
+
+done
+
+
---
+++ jni_notifications.rs
@@ -0,0 +1,77 @@
+// This is the interface to the JVM that we'll
+// call the majority of our methods on.
+use jni::JNIEnv;
+
+// These objects are what you should use as arguments to your native function.
+// They carry extra lifetime information to prevent them escaping from the
+// current local frame (which is the scope within which local (temporary)
+// references to Java objects remain valid)
+use jni::objects::{GlobalRef, JClass, JObject, JString};
+
+// use jni::objects::JByteArray;
+// use jni::sys::{jint, jlong};
+
+use std::{sync::mpsc, thread, time::Duration};
+
+// This `#[no_mangle]` keeps rust from "mangling" the name and making it unique
+// for this crate. The name follow a strict naming convention so that the
+// JNI implementation will be able to automatically find the implementation
+// of a native method based on its name.
+//
+// The `'local` lifetime here represents the local frame within which any local
+// (temporary) references to Java objects will remain valid.
+//
+// It's usually not necessary to explicitly name the `'local` input lifetimes but
+// in this case we want to return a reference and show the compiler what
+// local frame lifetime it is associated with.
+//
+// Alternatively we could instead return the `jni::sys::jstring` type instead
+// which would represent the same thing as a raw pointer, without any lifetime,
+// and at the end use `.into_raw()` to convert a local reference with a lifetime
+// into a raw pointer.
+#[no_mangle]
+pub extern "system" fn Java_HelloWorld_hello<'local>(
+    // Notice that this `env` argument is mutable. Any `JNIEnv` API that may
+    // allocate new object references will take a mutable reference to the
+    // environment.
+    mut env: JNIEnv<'local>,
+    // this is the class that owns our static method. Not going to be used, but
+    // still needs to have an argument slot
+    _class: JClass<'local>,
+    input: JString<'local>,
+) -> JString<'local> {
+
+
+    // Init libnotify
+    libnotify::init("myapp").unwrap();
+    // Create a new notification (doesn't show it yet)
+    let n = libnotify::Notification::new("Summary",
+                                         Some("Optional Body"),
+                                         None);
+    // Show the notification
+    n.show().unwrap();
+    // Update the existent notification
+    n.update("I am another notification", None, None).unwrap();
+    // Show the updated notification
+    n.show().unwrap();
+    // We are done, deinit
+    libnotify::uninit();
+
+
+
+
+
+    // First, we have to get the string out of java. Check out the `strings`
+    // module for more info on how this works.
+    let input: String = env
+        .get_string(&input)
+        .expect("Couldn't get java string!")
+        .into();
+
+    // Then we have to create a new java string to return. Again, more info
+    // in the `strings` module.
+    let output = env
+        .new_string(format!("Hello, {}!", input))
+        .expect("Couldn't create java string!");
+    output
+}
