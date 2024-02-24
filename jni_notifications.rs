2b8ddf72341ecd89932854f57ce8d1d0c8f7c1a0
thomas725 <thomas85@mail.ch>
2024-02-24 19:41:00 +0100

    rust version created and tested

--- jni_notifications.rs
+++ jni_notifications.rs
@@ -1,3 +1,4 @@
+use jni::sys::jint;
 // This is the interface to the JVM that we'll
 // call the majority of our methods on.
 use jni::JNIEnv;
@@ -6,12 +7,12 @@ use jni::JNIEnv;
 // They carry extra lifetime information to prevent them escaping from the
 // current local frame (which is the scope within which local (temporary)
 // references to Java objects remain valid)
-use jni::objects::{GlobalRef, JClass, JObject, JString};
+use jni::objects::{JClass, JString};
 
 // use jni::objects::JByteArray;
 // use jni::sys::{jint, jlong};
 
-use std::{sync::mpsc, thread, time::Duration};
+const GLOBAL_VERSION_STRING: &'static str = "0.99.2-RS";
 
 // This `#[no_mangle]` keeps rust from "mangling" the name and making it unique
 // for this crate. The name follow a strict naming convention so that the
@@ -30,7 +31,22 @@ use std::{sync::mpsc, thread, time::Duration};
 // and at the end use `.into_raw()` to convert a local reference with a lifetime
 // into a raw pointer.
 #[no_mangle]
-pub extern "system" fn Java_HelloWorld_hello<'local>(
+pub extern "system" fn Java_com_zoffcc_applications_jninotifications_NTFYActivity_jninotifications_1version<'local>(
+    // Notice that this `env` argument is mutable. Any `JNIEnv` API that may
+    // allocate new object references will take a mutable reference to the
+    // environment.
+    env: JNIEnv<'local>,
+    // this is the class that owns our static method. Not going to be used, but
+    // still needs to have an argument slot
+    _class: JClass<'local>,
+    // input: JString<'local>,
+) -> JString<'local> {
+    env.new_string(GLOBAL_VERSION_STRING)
+        .expect("Couldn't create java string!")
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_zoffcc_applications_jninotifications_NTFYActivity_jninotifications_1notify<'local>(
     // Notice that this `env` argument is mutable. Any `JNIEnv` API that may
     // allocate new object references will take a mutable reference to the
     // environment.
@@ -38,40 +54,49 @@ pub extern "system" fn Java_HelloWorld_hello<'local>(
     // this is the class that owns our static method. Not going to be used, but
     // still needs to have an argument slot
     _class: JClass<'local>,
-    input: JString<'local>,
-) -> JString<'local> {
+    application: JString<'local>,
+    title: JString<'local>,
+    message: JString<'local>,
+    icon_filename_fullpath: JString<'local>,
+) -> jint {
+    let ret: jint = 0;
 
+    if (application.is_null()) || (title.is_null()) || (message.is_null()) {
+        return -1;
+    }
+
+    let app_name: String = match env.get_string(&application) {
+        Ok(v) => v.into(),
+        Err(_) => return -1,
+    };
+    let summary: String = match env.get_string(&title) {
+        Ok(v) => v.into(),
+        Err(_) => return -1,
+    };
+    let body: String = match env.get_string(&message) {
+        Ok(v) => v.into(),
+        Err(_) => return -1,
+    };
 
     // Init libnotify
-    libnotify::init("myapp").unwrap();
-    // Create a new notification (doesn't show it yet)
-    let n = libnotify::Notification::new("Summary",
-                                         Some("Optional Body"),
-                                         None);
+    libnotify::init(&app_name).unwrap();
+    let n: libnotify::Notification;
+
+    if !icon_filename_fullpath.is_null() {
+        let icon: String = match env.get_string(&icon_filename_fullpath) {
+            Ok(v) => v.into(),
+            Err(_) => return -1,
+        };
+        n = libnotify::Notification::new(&summary, Some(body.as_str()), Some(icon.as_str()));
+    } else {
+        n = libnotify::Notification::new(&summary, Some(body.as_str()), None);
+    }
+
     // Show the notification
     n.show().unwrap();
-    // Update the existent notification
-    n.update("I am another notification", None, None).unwrap();
-    // Show the updated notification
-    n.show().unwrap();
+
     // We are done, deinit
     libnotify::uninit();
 
-
-
-
-
-    // First, we have to get the string out of java. Check out the `strings`
-    // module for more info on how this works.
-    let input: String = env
-        .get_string(&input)
-        .expect("Couldn't get java string!")
-        .into();
-
-    // Then we have to create a new java string to return. Again, more info
-    // in the `strings` module.
-    let output = env
-        .new_string(format!("Hello, {}!", input))
-        .expect("Couldn't create java string!");
-    output
+    ret
 }
