15c1ee31af28a4cca7d0dbe6aa22075cba34844e
Zoff <zoff99@users.noreply.github.com>
2024-03-06 21:17:46 +0100

    add error handling on NULLs

--- jni_notifications_macos.m
+++ jni_notifications_macos.m
@@ -20,21 +20,29 @@ JNIEXPORT jint JNICALL
 Java_com_zoffcc_applications_jninotifications_NTFYActivity_jninotifications_1notify(JNIEnv *env, jobject thiz,
     jstring application, jstring title, jstring message, jstring icon_filename_fullpath)
 {
+    if (title == NULL) {
+        return -1;
+    }
+    if (message == NULL) {
+        return -1;
+    }
     const char *title_cstr = (*env)->GetStringUTFChars(env, title, NULL);
     const char *message_cstr = (*env)->GetStringUTFChars(env, message, NULL);
-    NSString *nsTitle = [NSString stringWithUTF8String:title_cstr];
-    NSString *nsMessage = [NSString stringWithUTF8String:message_cstr];
-
-    NSUserNotification *userNotification = [[NSUserNotification alloc] init];
-    userNotification.title = nsTitle;
-    userNotification.informativeText = nsMessage;
-
-    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:userNotification];
+    if ((title_cstr != NULL) && (message_cstr != NULL)) {
+        NSString *nsTitle = [NSString stringWithUTF8String:title_cstr];
+        NSString *nsMessage = [NSString stringWithUTF8String:message_cstr];
+    
+        NSUserNotification *userNotification = [[NSUserNotification alloc] init];
+        userNotification.title = nsTitle;
+        userNotification.informativeText = nsMessage;
+    
+        [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:userNotification];
+    }
 
     (*env)->ReleaseStringUTFChars(env, title, title_cstr);
     (*env)->ReleaseStringUTFChars(env, message, message_cstr);
 
-    // NSLog(@"Hello from Objective-C");
+    // NSLog(@"jninotifications:Objective-C");
 
     return 0;
 }
