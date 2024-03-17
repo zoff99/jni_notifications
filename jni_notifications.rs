ad6c46405e3b7623d11cee0dd419de8161c0cdda
zoff99 <zoff99@users.noreply.github.com>
2024-03-17 13:13:48 +0100

    cleanup the rust version

--- jni_notifications.rs
+++ jni_notifications.rs
@@ -1,13 +1,9 @@
-use jni::sys::jint;
-use jni::JNIEnv;
-use jni::objects::{JClass, JString};
-
 const GLOBAL_VERSION_STRING: &'static str = "0.99.2-RS";
 
-// This `#[no_mangle]` keeps rust from "mangling" the name and making it unique
-// for this crate. The name follow a strict naming convention so that the
-// JNI implementation will be able to automatically find the implementation
-// of a native method based on its name.
+use jni::JNIEnv;
+use jni::objects::{JClass, JString};
+use jni::sys::jint;
+
 #[no_mangle]
 pub extern "system" fn Java_com_zoffcc_applications_jninotifications_NTFYActivity_jninotifications_1version<'local>(
     env: JNIEnv<'local>,
@@ -26,27 +22,49 @@ pub extern "system" fn Java_com_zoffcc_applications_jninotifications_NTFYActivit
     message: JString<'local>,
     icon_filename_fullpath: JString<'local>,
 ) -> jint {
-    let ret: jint = 0;
-
-    if (application.is_null()) || (title.is_null()) || (message.is_null()) {
+    if application.is_null() {
+        eprintln!("JString application was null!");
+        return -1;
+    }
+    if title.is_null() {
+        eprintln!("JString title was null!");
+        return -1;
+    }
+    if message.is_null() {
+        eprintln!("JString message was null!");
         return -1;
     }
 
     let app_name: String = match env.get_string(&application) {
         Ok(v) => v.into(),
-        Err(_) => return -1,
+        Err(e) => {
+            eprintln!("couldn't read JString application! {e}");
+            return -1;
+        }
     };
     let summary: String = match env.get_string(&title) {
         Ok(v) => v.into(),
-        Err(_) => return -1,
+        Err(e) => {
+            eprintln!("couldn't read JString title! {e}");
+            return -1;
+        }
     };
     let body: String = match env.get_string(&message) {
         Ok(v) => v.into(),
-        Err(_) => return -1,
+        Err(e) => {
+            eprintln!("couldn't read JString message! {e}");
+            return -1;
+        }
     };
 
+    let mut ret = 0;
+
     // Init libnotify
-    libnotify::init(&app_name).unwrap();
+    if let Err(e) = libnotify::init(&app_name) {
+        eprintln!("libnotify::init failed! {e}");
+        ret = -3;
+    }
+
     let n: libnotify::Notification;
 
     if !icon_filename_fullpath.is_null() {
@@ -60,7 +78,10 @@ pub extern "system" fn Java_com_zoffcc_applications_jninotifications_NTFYActivit
     }
 
     // Show the notification
-    n.show().unwrap();
+    if let Err(e) = n.show() {
+        eprintln!("libnotify::Notification::show failed! {e}");
+        ret = -2;
+    }
 
     // We are done, deinit
     libnotify::uninit();
