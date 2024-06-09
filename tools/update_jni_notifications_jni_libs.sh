fc09fa14a776b296a043060746483dc237f5b5d8
zoff99 <zoff99@users.noreply.github.com>
2024-06-09 14:48:26 +0200

    download also arm macos jni lib

--- tools/update_jni_notifications_jni_libs.sh
+++ tools/update_jni_notifications_jni_libs.sh
@@ -2,18 +2,10 @@
 base_url='https://github.com/zoff99/jni_notifications/releases/download/nightly/'
 
 file_mac='libjni_notifications.jnilib'
+file_arm_mac='libjni_notifications_arm64.jnilib'
 file_lix='libjni_notifications.so'
 file_lix_rust='libjni_notifications_rs.so'
 file_rpi='libjni_notifications_raspi.so'
-
-
-47 KB
-2 days ago
-
-39 KB
-2 days ago
-
-
 
 _HOME2_=$(dirname $0)
 export _HOME2_
@@ -24,6 +16,7 @@ basedir="$_HOME_""/../"
 
 cd "$basedir"
 wget "$base_url""$file_mac" -O "$file_mac"
+wget "$base_url""$file_arm_mac" -O "$file_arm_mac"
 wget "$base_url""$file_lix" -O "$file_lix"
 wget "$base_url""$file_lix_rust" -O "$file_lix_rust"
 wget "$base_url""$file_rpi" -O "$file_rpi"
