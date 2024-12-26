170475d7f131be25050e0bdeb63df18f8ec19788
zoff99 <zoff99@users.noreply.github.com>
2024-12-26 14:54:53 +0100

    add ASAN lib

--- tools/update_jni_notifications_jni_libs.sh
+++ tools/update_jni_notifications_jni_libs.sh
@@ -4,6 +4,7 @@ base_url='https://github.com/zoff99/jni_notifications/releases/download/nightly/
 file_mac='libjni_notifications.jnilib'
 file_arm_mac='libjni_notifications_arm64.jnilib'
 file_lix='libjni_notifications.so'
+file_asan_lix='libjni_notifications.so__ASAN'
 file_lix_rust='libjni_notifications_rs.so'
 file_rpi='libjni_notifications_raspi.so'
 
@@ -18,5 +19,6 @@ cd "$basedir"
 wget "$base_url""$file_mac" -O "$file_mac"
 wget "$base_url""$file_arm_mac" -O "$file_arm_mac"
 wget "$base_url""$file_lix" -O "$file_lix"
+wget "$base_url""$file_asan_lix" -O "$file_asan_lix"
 wget "$base_url""$file_lix_rust" -O "$file_lix_rust"
 wget "$base_url""$file_rpi" -O "$file_rpi"
